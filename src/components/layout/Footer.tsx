768be90c964e9b85a0a4a2963e75b3f6c84b7ebb
Priyankasapp <priyanka.s@applocum.org>
2026-06-18 12:20:15 +0530

    initial commit

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,10 +2,13 @@
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+   <footer className="w-full p-6 border-t bg-[#3D2A24]">
+      <div className="container mx-auto">
+        Footer
+      </div>
+    </footer>
   )
 }
 
 export default Footer
+
